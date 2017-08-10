cbfb2fe7f92108bd5bc7a340a81edb555f27786f
Shweta Danej <shwetadanej@gmail.com>
2017-08-10 16:21:46 +0530

    Updated Content

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('CONSUMER_KEY', 'YOUR CONSUMER_KEY'); 
-define('CONSUMER_SECRET', 'YOUR CONSUMER_SECRET');
-define('ACCESS_TOKEN', 'YOUR ACCESS_TOKEN');
-define('ACCESS_SECRET', 'YOUR ACCESS_SECRET');
-define('OAUTH_CALLBACK', 'YOUR Callback url');
+define('CONSUMER_KEY', 'jzUQiZ5nr2EeWuIgegpa7EllJ'); 
+define('CONSUMER_SECRET', 'Lz33v4Oh93oLHCPAqi2TDmWzgPWOVPkio75rZhIVETq6Exkf1S');
+define('ACCESS_TOKEN', '3292667492-ebGyvQUEK2t2AVg8vqFRWc9I7g9cnKQrwiHhNJ6');
+define('ACCESS_SECRET', 'UIDkEATt0Hojd8ZnhfYm6rgsmxH9TjH1pXLNZKwP2iM24');
+define('OAUTH_CALLBACK', 'http://beta.technonic.in/TwitterApp/home.php');
